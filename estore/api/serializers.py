67148375c5752c5e28ec66488fd1f2677c44d908
Asef <asef.quliyev2001@gmail.com>
2023-10-09 00:26:49 +0300

    Serializers added for related models #1

--- estore/api/serializers.py
+++ estore/api/serializers.py
@@ -1,3 +1,19 @@
 from rest_framework import serializers
 from estore.models import Product,Brand,Category
 
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    category=serializers.StringRelatedField(many=True)
+    brand=serializers.StringRelatedField(many=True)
+    class Meta:
+        model = Product
+        fields = '__all__'
--- estore/models.py
+++ estore/models.py
@@ -26,12 +26,12 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(
         max_digits=10, decimal_places=2, blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='category')
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     stock_quantity = models.PositiveIntegerField(blank=True, null=True)
     size = models.CharField(max_length=80, blank=True, null=True)
     color = models.CharField(max_length=80, blank=True, null=True)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE,related_name='brand')
     is_active = models.BooleanField(
         default=True)
     created_at = models.DateTimeField(auto_now_add=True)
